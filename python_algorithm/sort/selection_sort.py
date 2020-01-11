7f48135c64c3ac70103fc5b96756a035e908f4f2
Tsugunni <tsugumiqqq@gmail.com>
2020-01-11 14:29:14 +0900

    delete something

--- python_algorithm/sort/selection_sort.py
+++ python_algorithm/sort/selection_sort.py
@@ -1,12 +1,6 @@
-from typing import List
-
-
-def sSort(nums: List[int]) -> List[int]:
+def sSort(nums: list) -> list:
     for i in range(len(nums) - 1):
         minNum = nums[i:].index(min(nums[i:]))
         nums[i], nums[i + minNum] = nums[i + minNum], nums[i]
 
     return nums
-
-
-print(sSort([5, 2, 6, 1, 6, 0]))
