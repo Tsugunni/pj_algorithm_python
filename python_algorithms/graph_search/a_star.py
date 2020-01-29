d39709114c9c8cff0aa667be3235b762cff02f08
sintaro0221 <dorichan0629@gmail.com>
2020-01-30 01:46:39 +0900

    [wip]

--- python_algorithms/graph_search/a_star.py
+++ python_algorithms/graph_search/a_star.py
@@ -2,67 +2,66 @@ import heapq
 import itertools
 
 
-def astar(init_pos: tuple, goal: tuple) -> list:
-    passed_list = [init_pos]
-    init_score = distance(passed_list) + heuristic(init_pos)
-    checked = {init_pos: init_score}
-    searching_heap = []
-    heapq.heappush(searching_heap, (init_score, passed_list))
+class AstarObject(object):
+    def astar(self, init_pos: tuple, goal: tuple) -> list:
+        """
+        >>> dungeon = [ \
+            "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",\
+            "OS  O     O     O         O          O",\
+            "O   O  O  O  O            O    OOOO GO",\
+            "O      O     O  O   OOOO  O    O  OOOO",\
+            "OOOOOOOOOOOOOOOOOO  O     O    O     O",\
+            "O                O  O     O          O",\
+            "O        OOO     O  O     OOOOOOOOO  O",\
+            "O  OO    O    OOOO  O     O      OO  O",\
+            "O   O    O          O     O  O   O   O",\
+            "O   OOO  O          O        O   O   O",\
+            "O        O          O        O       O",\
+            "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",\
+            ]
+        >>> ast = AstarObject()
+        >>> init = ast.find_ch("S")
+        >>> goal = ast.find_ch("G")
+        >>> path = ast.astar(init, goal)
+        """
+        passed_list = [init_pos]
+        init_score = self.distance(passed_list) + self.heuristic(init_pos)
+        checked = {init_pos: init_score}
+        searching_heap = []
+        heapq.heappush(searching_heap, (init_score, passed_list))
 
-    while len(searching_heap) > 0:
-        score, passed_list = heapq.heappop(searching_heap)
-        last_passed_pos = passed_list[-1]
+        while len(searching_heap) > 0:
+            score, passed_list = heapq.heappop(searching_heap)
+            last_passed_pos = passed_list[-1]
 
-        if last_passed_pos == goal:
-            return passed_list
+            if last_passed_pos == goal:
+                return passed_list
 
-        for pos in nexts(last_passed_pos):
-            new_passed_list = passed_list + [pos]
-            pos_score = distance(new_passed_list) + heuristic(pos)
+            for pos in self.nexts(last_passed_pos):
+                new_passed_list = passed_list + [pos]
+                pos_score = self.distance(new_passed_list) + self.heuristic(pos)
 
-            if pos in checked and checked[pos] <= pos_score:
-                continue
+                if pos in checked and checked[pos] <= pos_score:
+                    continue
 
-            checked[pos] = pos_score
-            heapq.heappush(searching_heap, (pos_score, new_passed_list))
+                checked[pos] = pos_score
+                heapq.heappush(searching_heap, (pos_score, new_passed_list))
 
-    return []
+        return []
 
+    def find_ch(ch: str) -> tuple:
+        for i, l in enumerate(dungeon):
+            for j, c in enumerate(l):
+                if c == ch:
+                    return (i, j)
 
-dungeon = [
-    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
-    "OS  O     O     O         O          O",
-    "O   O  O  O  O            O    OOOO GO",
-    "O      O     O  O   OOOO  O    O  OOOO",
-    "OOOOOOOOOOOOOOOOOO  O     O    O     O",
-    "O                O  O     O          O",
-    "O        OOO     O  O     OOOOOOOOO  O",
-    "O  OO    O    OOOO  O     O      OO  O",
-    "O   O    O          O     O  O   O   O",
-    "O   OOO  O          O        O   O   O",
-    "O        O          O        O       O",
-    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
-]
+    def nexts(pos: tuple):
+        wall = "O"
 
-
-def find_ch(ch: str) -> tuple:
-    for i, l in enumerate(dungeon):
-        for j, c in enumerate(l):
-            if c == ch:
-                return (i, j)
-
-
-init = find_ch("S")
-goal = find_ch("G")
-
-
-def nexts(pos: tuple):
-    wall = "O"
-
-    for a, b in itertools.product([" + 1", " - 1", ""], repeat=2):
-        if a or b:
-            if dungeon[eval("pos[0]" + a)][eval("pos[1]" + b)] != wall:
-                yield (eval("pos[0]" + a), eval("pos[1]" + b))
+        for a, b in itertools.product([" + 1", " - 1", ""], repeat=2):
+            if a or b:
+                if dungeon[eval("pos[0]" + a)][eval("pos[1]" + b)] != wall:
+                    yield (eval("pos[0]" + a), eval("pos[1]" + b))
 
 
 def heuristic(pos: tuple) -> float:
@@ -84,14 +83,17 @@ def render_path(path: list) -> list:
     return ["".join(l) for l in buf]
 
 
-# Run example
-path = astar(init, goal)
+if __name__ == "__main__":
+    import doctest
 
-if len(path) > 0:
-    print("\n".join(render_path(path)))
+    doctest.testmod()
+# # Run example
+# path = astar(init, goal)
 
-else:
-    print("failed")
+# if len(path) > 0:
+#     print("\n".join(render_path(path)))
+# else:
+#     print("failed")
 
 
 # Run result
