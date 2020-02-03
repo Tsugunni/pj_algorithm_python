69a344a3ccd757e9a42f017f2918fdd6c9af1a32
Tsugunni <tsugumiqqq@gmail.com>
2020-02-04 00:41:47 +0900

    add run example and result

--- python_algorithm/sort/selection_sort.py
+++ python_algorithm/sort/selection_sort.py
@@ -1,6 +1,15 @@
-def sSort(nums: list) -> list:
+def selectionSort(nums: list) -> list:
     for i in range(len(nums) - 1):
         minNum = nums[i:].index(min(nums[i:]))
         nums[i], nums[i + minNum] = nums[i + minNum], nums[i]
 
     return nums
+
+
+# Run example
+nums = [5, 8, 2, 4, 1, 0]
+print(selectionSort(nums))
+
+
+# Run result
+# [0, 1, 2, 4, 5, 8]
