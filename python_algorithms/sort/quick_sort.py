a5e6742e664ef71fe034d3ba4108b9ecf1966295
Tsugunni <tsugumiqqq@gmail.com>
2020-02-03 23:01:10 +0900

    add run example and result

--- python_algorithms/sort/quick_sort.py
+++ python_algorithms/sort/quick_sort.py
@@ -7,3 +7,12 @@ def quickSort(nums: list) -> list:
     r = [x for x in nums[:-1] if x > p]
 
     return quickSort(l) + [p] + quickSort(r)
+
+
+# Run example
+nums = [5, 8, 2, 4, 1, 0]
+print(quickSort(nums))
+
+
+# Run result
+# [0, 1, 2, 4, 5, 8]
